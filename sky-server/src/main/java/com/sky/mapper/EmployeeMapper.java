ddd2950443297095090a8b357899ebf76190a200
JACK <1900235804@qq.com>
2024-10-11 16:37:48 +0800

    Second update

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,10 +1,13 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
 import com.sky.dto.EmployeeDTO;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 @Mapper
 public interface EmployeeMapper {
@@ -20,5 +23,7 @@ public interface EmployeeMapper {
     @Insert("INSERT INTO employee (name, username, password, phone, sex, id_number, status, create_time, update_time, create_user, update_user )VALUES(#{name}, #{username}, #{password}, #{phone}, #{sex}, #{idNumber}, #{status}, #{createTime}, #{updateTime}, #{createUser}, #{updateUser})")
     void save(Employee employee);
 
+    Page<Employee> pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
 
+    void startorStop(Employee employee);
 }
--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -1,16 +1,21 @@
 package com.sky.service.impl;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import com.sky.constant.MessageConstant;
 import com.sky.constant.PasswordConstant;
 import com.sky.constant.StatusConstant;
 import com.sky.context.BaseContext;
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import com.sky.exception.AccountLockedException;
 import com.sky.exception.AccountNotFoundException;
 import com.sky.exception.PasswordErrorException;
 import com.sky.mapper.EmployeeMapper;
+import com.sky.result.PageResult;
+import com.sky.result.Result;
 import com.sky.service.EmployeeService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +25,7 @@ import org.springframework.util.DigestUtils;
 
 import javax.xml.crypto.Data;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
@@ -62,6 +68,13 @@ public class EmployeeServiceImpl implements EmployeeService {
         return employee;
     }
 
+    public PageResult pageQuery(EmployeePageQueryDTO employeePageQueryDTO){
+        PageHelper.startPage(employeePageQueryDTO.getPage(),employeePageQueryDTO.getPageSize());
+        Page<Employee> employees = employeeMapper.pageQuery(employeePageQueryDTO);
+        long total = employees.getTotal();
+        List<Employee> result = employees.getResult();
+        return new PageResult(total,result);
+    }
 
     public Employee save(EmployeeDTO employeeDTO) {
         Employee employee = new Employee();
@@ -78,4 +91,11 @@ public class EmployeeServiceImpl implements EmployeeService {
         return employee;
     }
 
+    public Employee startorStop(Integer status,Long id){
+        Employee employee=new Employee();
+        employee.setId(id);
+        employee.setStatus(status);
+        employeeMapper.startorStop(employee);
+        return employee;
+    }
 }
